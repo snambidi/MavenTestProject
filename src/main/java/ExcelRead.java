af7a576f505e61ce0590728331e07375eef4486f
snambidi <sarithanambidi87@gmail.com>
2024-10-16 22:04:00 +0530

    changes added to excelread

---
+++ src/main/java/ExcelRead.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelRead {
+
+	XSSFSheet sheet;
+	ExcelRead() throws IOException{
+		FileInputStream inputfile =new FileInputStream("D:\\JavaSamples\\MyExcel.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(inputfile);
+		sheet = workbook.getSheet("Sheet1");
+	}
+	public String readExcelData(int i,int j) {
+		XSSFRow row = sheet.getRow(i);
+		Cell cell = row.getCell(j);
+	 
+	return cell.getStringCellValue();
+
+    }
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		ExcelRead ex = new ExcelRead();
+		String s = ex.readExcelData(0, 0);
+		System.out.println("EXCEL DATA:"+ s);
+
+	}
+}
+
+
